b345e5f14a2b88f4523e23fbf1f97d900a0779fa
Aleksei Usov <usalekse@gmail.com>
2019-12-29 19:35:20 +0300

    4.4. Максимум из трех чисел[#207473]

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -2,6 +2,6 @@ package ru.job4j.condition;
 
 public class Triangle {
     public static boolean exist (double ab, double bc, double cd) {
-        return (ab+bc>cd)&&(bc+cd>ab)&&(cd+ab>bc)? true:false;
+        return (ab+bc>cd)&&(bc+cd>ab)&&(cd+ab>bc);
     }
 }
