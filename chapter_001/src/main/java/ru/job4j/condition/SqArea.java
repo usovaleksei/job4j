daf8ec3f4112497e4aa885c8e01240d76fd9843e
Aleksei Usov <usalekse@gmail.com>
2020-01-02 21:43:00 +0300

    4.5. Отладка программы в IDEA[#207471]

--- chapter_001/src/main/java/ru/job4j/condition/SqArea.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqArea.java
@@ -4,7 +4,8 @@ public class SqArea {
     public static double square(int p, int k) {
         double height = p/(2 * (k + 1));
         double length = height * k;
-        return height * length;
+        double s = height * length;
+        return s;
     }
     public static void main(String[] args) {
         double result1 = square(6, 2);
