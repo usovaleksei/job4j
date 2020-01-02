3cea7b2883d9e67672607b92c74608c93f1e082c
Aleksei Usov <usalekse@gmail.com>
2020-01-03 00:33:31 +0300

    5.4. Протеиновая диета.[#207534]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nik) {
+        int month = 0;
+        while (ivan < nik) {
+            month++;
+            nik*=2;
+            ivan*=3;
+        }
+        return month;
+    }
+}
