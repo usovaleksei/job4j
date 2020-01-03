23197d2d22f395445c6d5449aade5963c0926b58
Aleksei Usov <usalekse@gmail.com>
2020-01-04 00:08:46 +0300

    5.7. Ипотека.[#207536]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public int year (int amount, int salary, double percent) {
+        int year = 0;
+        double bal = amount;
+        while (bal > 0) {
+            year++;
+            bal = bal + bal*(percent/100) - salary;
+            }
+        return year;
+    }
+}
