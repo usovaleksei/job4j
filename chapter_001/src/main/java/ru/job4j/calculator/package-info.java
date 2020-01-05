1e288d4d3554e3fc336f18c9eed4f55f3927e65b
Aleksei Usov <usalekse@gmail.com>
2020-01-05 15:27:03 +0300

    5.9. Крест в псевдографике. [#207538]

--- chapter_001/src/main/java/ru/job4j/calculator/package-info.java
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -5,4 +5,4 @@
 * @version $Id$
 * @since 0.1
 */
-package ru.job4j.calculate;
+package ru.job4j.calculator;
---
+++ chapter_001/src/main/java/ru/job4j/loop/Slash.java
@@ -0,0 +1,34 @@
+package ru.job4j.loop;
+
+/**
+ * Class  Класс для рисования креста в консоли.
+ * @author Aleksei Usov
+ * @since 05.01.2020
+ * @version 1.0
+ */
+
+public class Slash {
+    public static void draw (int size) {
+        int temp = size - 1;
+        for (int row = 0; row < size; row++) {
+            for (int cell = 0; cell < size; cell++) {
+                boolean left = (cell == row);
+                boolean right = (temp == (cell+row));
+                if (left) {
+                    System.out.print ("0");
+                } else if (right) {
+                    System.out.print ("0");
+                }
+                    else {
+                        System.out.print (" ");
+                    }
+                }
+            System.out.println();
+            }
+        }
+
+    public static void main (String[] args) {
+        System.out.println("Draw by 5");
+        draw (5);
+    }
+}
