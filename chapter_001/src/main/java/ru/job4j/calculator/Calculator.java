579a8b810de8c18949f316f8f97451de0acb4eb3
Aleksei Usov <usalekse@gmail.com>
2020-01-05 14:27:42 +0300

    5.8. JavaDoc. Code convention[#207542]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,5 +1,12 @@
 package ru.job4j.calculator;
 
+/**
+ * Class  Класс для вычисления арифмитических операций "сложение", "вычитание", "умножение", "деление".
+ * @author Aleksei Usov
+ * @since 05.01.2020
+ * @version 1.0
+ */
+
 public class Calculator {
     public static void add(double first, double second) {
         double result = first + second;
@@ -17,6 +24,12 @@ public class Calculator {
         double result = first - second;
         System.out.println(first + " - " + second + " = " + result);
     }
+
+    /**
+     * Main.
+     * @param args - args.
+     */
+
     public static void main (String[] args) {
         add(1, 1);
         div(4, 2);
