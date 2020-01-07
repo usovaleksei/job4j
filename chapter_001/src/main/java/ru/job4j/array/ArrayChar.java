fe26e4d508189c7639adbef312b3927fff1c9e29
Aleksei Usov <usalekse@gmail.com>
2020-01-08 00:29:38 +0300

    6.4.1. Слово заканчивается с ...[#207484]

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
-        for (int i=0; i != pref.length; i++) {
+        for (int i = 0; i != pref.length; i++) {
             if (pref[i] != word[i]) {
                 result = false;
                 break;
