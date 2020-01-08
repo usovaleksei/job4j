acb9134d62c3866bad899d0b26055919da2a23e4
Aleksei Usov <usalekse@gmail.com>
2020-01-08 22:35:45 +0300

    6.5. Поиск индекса в диапазоне.[#207485]

--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -35,4 +35,28 @@ public class FindLoopTest {
         int expect = -1;
         assertThat (result, is(expect));
     }
+
+    //тест, когда есть указанный элемент в заданном диапазоне индексов массива
+    @Test
+    public void whenFind2() {
+        int[] input = new int[]{5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    //тест, когда указанный элемент лежит вне зоны заданного диапазона индексов массива
+    @Test
+    public void whenNotFind5() {
+        int[] input = new int[]{5, 2, 10, 2, 4, 11, 17};
+        int value = 5;
+        int start = 1;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
 }
