0e469290eecd40bf1b6492e220c6784409472dd1
Aleksei Usov <usalekse@gmail.com>
2020-02-02 23:07:45 +0300

    8.1 Экзамен. Базовый синтаксис. Объединение двух массивов [#207529]

---
+++ chapter_001/src/main/java/ru/job4j/sort/Merge.java
@@ -0,0 +1,39 @@
+package ru.job4j.sort;
+
+import java.util.Arrays;
+
+public class Merge {
+    public int[] merge(int[] left, int[] right) {
+        int[] rsl = new int[left.length + right.length];
+        int i = 0, j = 0, k = 0;
+        while (k != (left.length + right.length)) {
+            if (i >= left.length) {
+                rsl[k] = right[j];
+                j++;
+                k++;
+            } else if (j >= right.length) {
+                rsl[k] = left[i];
+                i++;
+                k++;
+            } else if (left[i] < right[j]) {
+                rsl[k] = left[i];
+                i++;
+                k++;
+            } else {
+                rsl[k] = right[j];
+                j++;
+                k++;
+            }
+        }
+            return rsl;
+        }
+
+    public static void main(String[] args) {
+        Merge process = new Merge();
+        int[] rsl = process.merge(
+                new int[] {1, 2, 4},
+                new int[] {1, 3}
+        );
+        System.out.println(Arrays.toString(rsl));
+    }
+}
