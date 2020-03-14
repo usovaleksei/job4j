49b06b5e4073e97ad6348860c7069fc0a4c81670
Aleksei Usov <usalekse@gmail.com>
2020-03-14 23:10:00 +0300

    2.2. Рефакторинг - Площадь треугольника.[#207618]

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -1,7 +1,67 @@
 package ru.job4j.condition;
 
 public class Triangle {
-    public static boolean exist(double ab, double bc, double cd) {
-        return (ab + bc > cd) && (bc + cd > ab) && (cd + ab > bc);
-    }
+
+        private Point first;
+        private Point second;
+        private Point third;
+
+        public Triangle(Point ap, Point bp, Point cp) {
+            this.first = ap;
+            this.second = bp;
+            this.third = cp;
+        }
+
+        /**
+         * Метод проверяет можно ли построить треугольник с такими длинами сторон.
+         *
+         *
+         * @param a Длина от точки a b.
+         * @param b Длина от точки a c.
+         * @param c Длина от точки b c.
+         * @return да или нет
+         */
+        public static boolean exist(double a, double b, double c) {
+            return (a + b > c) && (b + c > a) && (c + a > b);
+        }
+
+        /**
+         * Метод вычисления периметра по длинам сторон.
+         *
+         * Формула.
+         *
+         * (a + b + c) / 2
+         *
+         * @param a расстояние между точками a b
+         * @param b расстояние между точками a c
+         * @param c расстояние между точками b c
+         * @return Периметр.
+         */
+        public double period(double a, double b, double c) {
+            return ((a + b + c) / 2);
+        }
+
+        /**
+         * Метод должен вычислить площадь треугольника.
+         *
+         * Формула.
+         *
+         * √ p *(p - ab) * (p - ac) * (p - bc)
+         *
+         * где √ - корень квадратный, для извлечения корня использовать метод Math.sqrt().
+         *
+         * @return Вернуть площадь, если треугольник существует или -1 (если не существует).
+         */
+        public double area() {
+            double rsl = -1;
+            double a = first.distance(second);
+            double b = first.distance(third);
+            double c = second.distance(third);
+            double p = period(a, b, c);
+            if (this.exist(a, b, c)) {
+                // формула для расчета площади треугольника.
+                rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+            }
+            return rsl;
+        }
 }
--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.condition;
 
+import org.junit.Assert;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -16,5 +17,16 @@ public class TriangleTest {
     public void whenNotExist() {
         boolean result = Triangle.exist(2, 8, 3);
         assertThat(result, is(false));
+    }
+    //проверка правильности расчета площади треугольника
+    @Test
+    public void trueArea() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        Point c = new Point(2, 2);
+        Triangle d = new Triangle(a, b, c);
+        double expected = 1.99;
+        double out = d.area();
+        Assert.assertEquals(out, expected, 0.01);
     }
 }
