8ca170f6ba5a29501f7418592d7a61b9978b9eca
Aleksei Usov <usalekse@gmail.com>
2020-04-28 00:42:56 +0300

    2.3.4. Удаление моделей из массива.[#207642]

---
+++ chapter_002/src/main/java/ru/job4j/oop/pojo/Shop.java
@@ -0,0 +1,46 @@
+package ru.job4j.oop.pojo;
+
+/**
+ * Class removing models from an array
+ * @author Aleksei Usov
+ */
+
+public class Shop {
+    public static void main(String[] args) {
+        Product[] products = new Product[5];
+        products[0] = new Product("milk", 10);
+        products[2] = new Product("bread", 4);
+        products[3] = new Product("egg", 19);
+
+        for (int index = 0; index < products.length; index++) {
+            Product prod = products[index];
+            if (prod != null) {
+                System.out.println(prod.getName() + " : " + prod.getCount());
+            } else {
+                System.out.println("null");
+            }
+        }
+        System.out.println();
+
+        delete(products, 1);
+        for (int index = 0; index < products.length; index++) {
+            Product prod = products[index];
+            if (prod != null) {
+                System.out.println(prod.getName() + " : " + prod.getCount());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+     }
+
+    //method to delete array cell
+    public static Product[] delete (Product[] products, int index) {
+        for (int i = index; i < products.length - 1; i++) {
+            products[i] = products[i + 1];
+        }
+        products[products.length - 1] = null;
+        return products;
+    }
+}
+
