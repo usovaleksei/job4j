dfaa531c57aad0c1ff2c80ccbc05f936fcb29c2d
Aleksei Usov <usalekse@gmail.com>
2020-07-28 23:28:48 +0300

    new readme, pom 28/07/2020

--- chapter_002/src/main/test/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/main/test/ru/job4j/tracker/StartUITest.java
@@ -1,42 +1,6 @@
 package ru.job4j.tracker;
 
-import org.junit.Test;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
-import static org.hamcrest.core.IsNull.nullValue;
-
 public class StartUITest {
-    @Test
-    public void whenAddItem() {
-        String[] answer = {"Fix PC"};
-        Input input = new StubInput(answer);
-        Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Fix PC");
-        assertThat(created.getName(), is(expected.getName()));
-    }
 
-    @Test
-    public void whenReplaceItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answer = {item.getId(), "replaced item"};
-        StartUI.replaceItem(new StubInput(answer), tracker);
-        Item replaced = tracker.findById(item.getId());
-        assertThat(replaced.getName(), is("replaced item"));
-    }
-
-    @Test
-    public void whenDeleteItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("delete item");
-        tracker.add(item);
-        String[] answer = {item.getId()};
-        StartUI.deleteItem(new StubInput(answer), tracker);
-        Item deleted = tracker.findById(item.getId());
-        assertThat(deleted, is(nullValue()));
-    }
 
 }
