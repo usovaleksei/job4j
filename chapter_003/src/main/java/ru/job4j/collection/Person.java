2ed4137bc3ac40b5026d22038045e94807b8336f
Aleksei Usov <usalekse@gmail.com>
2020-09-23 23:53:17 +0300

    3.1.1. Телефонный справочник на базе ArrayList [#207565]

---
+++ chapter_003/src/main/java/ru/job4j/collection/Person.java
@@ -0,0 +1,31 @@
+package ru.job4j.collection;
+
+public class Person {
+    private String name;
+    private String surname;
+    private String phone;
+    private String address;
+
+    public Person(String name, String surname, String phone, String adress) {
+        this.name = name;
+        this.surname = surname;
+        this.phone = phone;
+        this.address = adress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
---
+++ chapter_003/src/test/ru/job4j/collections/PhoneDictionaryTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.collections;
+
+import org.junit.Test;
+import ru.job4j.collection.Person;
+import ru.job4j.collection.PhoneDictionary;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+}
